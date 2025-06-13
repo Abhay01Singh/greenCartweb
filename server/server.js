9b64131ca53a86fad646e4aff82818e2a460a0a6
Abhay <abhay@gmail.com>
2025-06-13 08:27:02 +0530

    remove localhost

--- server/server.js
+++ server/server.js
@@ -19,10 +19,7 @@ await connectDB();
 await connectCloudinary();
 
 // allow multiple origins
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://greencart-sigma-three.vercel.app",
-];
+const allowedOrigins = ["https://greencart-sigma-three.vercel.app"];
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
