053393b358d2b7d31e2254fb69bf5d517590be1e
Abhay <abhay@gmail.com>
2025-06-13 12:15:51 +0530

    change

--- server/server.js
+++ server/server.js
@@ -19,10 +19,7 @@ await connectDB();
 await connectCloudinary();
 
 // allow multiple origins
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://greencart-livid.vercel.app",
-];
+const allowedOrigins = ["http://localhost:5173"];
 
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 
