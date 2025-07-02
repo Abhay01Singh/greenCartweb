2b7a2f9ca939abd378de700129f74c4c3af5c151
Abhay <abhay@gmail.com>
2025-07-02 10:04:55 +0530

    update url

--- server/server.js
+++ server/server.js
@@ -19,7 +19,10 @@ await connectDB();
 await connectCloudinary();
 
 // allow multiple origins
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://greencartweb-rhbw.onrender.com",
+];
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
